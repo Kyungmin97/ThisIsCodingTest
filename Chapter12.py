08b097b0a07bba018252bdde392b8c45125f8562
Kyungmin97 <87704434+Kyungmin97@users.noreply.github.com>
2022-05-23 02:40:20 +0000

    Chpater12

--- Chapter12.py
+++ Chapter12.py
@@ -1,2 +1,43 @@
-
+"""
 print("07. 럭키 스트레이트")
+
+s=""
+s=input()
+half=len(s)//2
+sum1=0
+for i in s[:half]:
+  sum1+=int(i)
+
+for i in s[half:]:
+  sum1-=int(i)
+
+if sum1==0:
+  print("LUCKY")
+else:
+  print("READY")
+"""
+"""
+print("08. 문자열 재정렬")
+
+s=""
+s2=[]
+s=input()
+sum=0
+for i in s:
+  if i >='0' and i<='9':
+    sum+=int(i)
+  else:
+    s2.append(i)
+s2.sort()
+print(''.join(s2),end=str(sum))
+"""
+
+print("09. 문자열 압축")
+
+
+
+
+
+
+
+
